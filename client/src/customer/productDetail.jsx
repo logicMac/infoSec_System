18074fc6c6aaaefae7e233584a473d57dbaad803
logicMac <maclovellates11@gmail.com>
2026-05-01 00:17:07 +0800

    update

--- client/src/customer/productDetail.jsx
+++ client/src/customer/productDetail.jsx
@@ -22,29 +22,29 @@ export default function ProductDetail() {
       <div>
          <CustomerNavbar/>
 
-            <form key={product.product_id} onSubmit={handleBuyProduct} className="flex flex-row justify-center items-center p-50 bg-gray-100">
-               <div className="bg-white w-100 p-5">
-                  <div className="flex justify-center items-center border border-gray-100 rounded-md">
-                     <img src={`http://localhost:3000/uploads/${product.image}`} alt="" className="h-100"/>
+            <form key={product.product_id} onSubmit={handleBuyProduct} className="flex flex-row justify-center items-start gap-5  bg-gray-100 h-screen">
+               <div className="flex flex-col justify-center items-center bg-white w-1/2 p-10 rounded-lg shadow-lg h-full">
+                  <div className="flex justify-center items-center border border-gray-100 rounded-md w-full h-150">
+                     <img src={`http://localhost:3000/uploads/${product.image}`} alt="" className="h-100 w-full object-contain"/>
                   </div>      
                
-                  <div className="flex flex-col justify-between items-start w-100 h-50 mt-5">
+                  <div className="flex flex-col justify-between items-start w-full mt-5">
                      <div className="p-2 space-y-5">   
                         <h1 className="text-2xl font-semibold">{product.product_name}</h1>  
                         <p className="text-md">{product.product_description}</p>   
-                        <p className="text-2xl font-semibold">{product.price}</p>         
+                        <p className="text-2xl font-semibold text-green-600">&#8369;{product.price}</p>         
                      </div>
                   </div>   
 
-                  <div className="flex flex-row justify-center items-center space-x-2 w-full">
-                     <button className="p-2 rounded-md w-full bg-black text-white hover:scale-105 transition duration-200">Buy now!</button>
-                     <button className="p-2 rounded-md w-full  bg-black text-white hover:scale-105 transition duration-200">Add to Cart</button>
+                  <div className="flex flex-row justify-center items-center space-x-2 w-full mt-5">
+                     <button type="submit" className="p-2 rounded-md w-full bg-black text-white hover:scale-105 transition duration-200">Buy now!</button>
+                     <button type="button" className="p-2 rounded-md w-full bg-black text-white hover:scale-105 transition duration-200">Add to Cart</button>
                   </div>   
                </div>   
 
-               <div className="flex justify-center items-center bg-white w-100 h-200">
+               <div className="flex justify-center items-center w-1/2 p-5 rounded-lg">
                   <div className="">
-                     <h1>Order summary</h1>
+                     <h1 className="text-xl font-semibold">Order summary</h1>
                   </div>
                </div>
             </form>
