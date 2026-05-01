51dbc5a6a2811523a490a04932ea2c2690502f1e
logicMac <maclovellates11@gmail.com>
2026-05-02 01:15:05 +0800

    near finsihed ordering logic

--- client/src/customer/productDetail.jsx
+++ client/src/customer/productDetail.jsx
@@ -3,9 +3,11 @@ import CustomerNavbar from "./navbar";
 import { useLocation } from "react-router-dom";
 import { orderProducts } from "../api/orderApi";
 import { getAuthData } from "../utils/authGetter";
+import COD from "../assets/COD.png";
 
 export default function ProductDetail() {
    const location = useLocation();
+   const [quantity, setQuantity] = useState(1);
    const product = location.state?.product;
    const token = getAuthData();
 
@@ -20,6 +22,18 @@ export default function ProductDetail() {
       }
    } 
 
+   const increment = () => {
+      setQuantity(quantity + 1);
+   }
+
+   const decrement = () => {
+      setQuantity(quantity - 1);
+
+      if (quantity <= 1) {
+         setQuantity(1);
+      }
+   }
+
    if (!product) return <div className="p-10 text-center">No product data found.</div>;
 
    return (
@@ -32,7 +46,7 @@ export default function ProductDetail() {
             className="flex flex-col md:flex-row gap-10 p-6 md:p-12 max-w-7xl mx-auto"
          >
             {/* LEFT SIDE: Product Display */}
-            <div className="flex flex-col w-full md:w-3/5 space-y-8">
+            <div className="flex flex-col  w-full md:w-3/5 space-y-8">
 
                <p>&#8592;</p>
                
@@ -44,20 +58,47 @@ export default function ProductDetail() {
 
                {/* Product Detail Card */}
                <div className="flex items-center justify-between gap-6 bg-white p-6 rounded-2xl shadow-sm border border-gray-200">
-                  <div className="h-32 w-32 bg-gray-50 rounded-xl overflow-hidden flex-shrink-0 border border-gray-100">
-                     <img 
-                        src={`http://localhost:3000/uploads/${product.image}`} 
-                        alt={product.product_name} 
-                        className="h-full w-full object-contain p-2"
-                     />
+                  <div className="flex flex-row justify-center items-center gap-4">
+                     <div className="h-32 w-32 bg-gray-50 rounded-xl overflow-hidden flex-shrink-0 border border-gray-100">
+                        <img 
+                           src={`http://localhost:3000/uploads/${product.image}`} 
+                           alt={product.product_name} 
+                           className="h-full w-full object-contain p-2"
+                        />
                   </div>
-                  <div className="flex flex-row">
-                     <div className="flex flex-col">
-                        <h2 className="text-xl font-semibold text-gray-800">{product.product_name}</h2>
-                        <p className="text-gray-400 text-sm italic">Product ID: {product.product_id}</p>
+                  <div className="flex flex-row">  
+                     <div className="flex flex-col space-y-2">
+                           <h2 className="text-xl font-semibold text-gray-800">{product.product_name}</h2>
+                           <p className="text-gray-400 text-sm italic">{product.product_description}</p>
+                           <div className="flex flex-row justify-start items-center gap-2"> 
+                              <p>Size:</p> 
+                              <select className="text-center p-1 border border-gray-300 rounded-md w-15">
+                              <option value="S">S</option>
+                              <option value="M">M</option>
+                              <option value="L">L</option>
+                              <option value="XL">XL</option>
+                              <option value="XXL">XXL</option>
+                           </select>
+                           </div>
                      </div>
                   </div>
+                  </div>
                   
+                  <div className="flex flex-row bg-gray-200 rounded-md justify-center items-center space-x-4">
+                     <button 
+                        onClick={decrement}
+                        className="p-1 border border-gray-100 rounded-md"
+                        >
+                           -
+                     </button>
+                     <p>{quantity}</p>
+                     <button 
+                        onClick={increment}
+                        className="p-1 border border-gray-100 rounded-md"
+                        >
+                           +
+                     </button>
+                  </div>
                </div>      
             </div>   
 
@@ -76,15 +117,36 @@ export default function ProductDetail() {
                   </div>
                   
                   <div className="pt-4 border-t border-gray-100">
+                     <div className="space-y-4">
+                        <h1 className="text-xl font-semibold">Payment method</h1>
+
+                        <div className="flex flex-col space-y-4">
+                           <div className="flex flex-row justify-between items-center">
+                              <div className="flex flex-row">
+                                 <img src={COD} alt="" className="w-10"/>
+                              <p>Cash on delivery</p>
+                              </div>
+                              <input type="radio" />
+                           </div>
+                        </div>
+
+                        <div className="flex flex-col space-y-4">
+                           <div className="flex flex-row justify-between items-center">
+                              <p>Maya</p>
+                              <input type="radio" />
+                           </div>
+                        </div>   
+                     </div>      
+
                      <div className="flex justify-between items-end">
                         <span className="text-gray-800 font-bold text-lg">Total</span>
-                        <span className="text-3xl font-black text-blue-600">&#8369;{product.price}</span>
+                        <span className="text-3xl font-black text-black">&#8369;{product.price}</span>
                      </div>
                   </div>
 
                   <button 
                      type="submit"
-                     className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-4 rounded-xl mt-6 transition-all active:scale-95 shadow-lg shadow-blue-200"
+                     className="w-full bg-black hover:scale-105 transition duration-200 text-white font-bold py-4 rounded-xl mt-6 active:scale-95 shadow-lg shadow-blue-200"
                   >
                      Confirm Purchase
                   </button>
